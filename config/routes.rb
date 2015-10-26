1432313b14e76ef3c1b1b468d700f5a3b204f435
James W Dickhoner <dickhoner@gmail.com>
2015-10-26 15:00:28 -0600

    Implemented Log In

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :views
   root "pages#home"
   get "about" => "pages#about" #creates about path
   get "maps" => "pages#maps"
   get "fish_book" => "pages#fish_book"
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
